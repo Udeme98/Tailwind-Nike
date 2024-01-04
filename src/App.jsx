8ca912b6cca5670e0788ba0baa31b0ca66bc5549
Udeme98 <100492222+Udeme98@users.noreply.github.com>
2024-01-04 13:11:18 +0100

    Building Nike site using React + TailwindCSS

---
+++ src/App.jsx
@@ -0,0 +1,7 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+export default function App() {
+  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+}
