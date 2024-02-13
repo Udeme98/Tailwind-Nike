d4b5c6d2264e46a45bb9a295d73cf797bc1f3ba9
Udeme98 <Darealudeme1@gmail.com>
2024-02-13 06:55:03 +0100

    completed the structure for the web app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,42 @@
+import {
+  Hero,
+  Footer,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+} from "./sections";
+
 export default function App() {
   return (
     <main className="relative">
       Nav
-      <section className="xl:padding-l wide:padding-r padding-b">Hero</section>
-      <section className="padding">Popular Products</section>
-      <section className="padding">Super Quality</section>
-      <section className="padding-x py-10">Services</section>
-      <section className="padding">Special Offers</section>
-      <section className="padding bg-pale-blue">Customer Review</section>
-      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
-      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+      <section className="xl:padding-l wide:padding-r padding-b">
+        <Hero />
+      </section>
+      <section className="padding">
+        <PopularProducts />
+      </section>
+      <section className="padding">
+        <SuperQuality />
+      </section>
+      <section className="padding-x py-10">
+        <Services />
+      </section>
+      <section className="padding">
+        <SpecialOffers />
+      </section>
+      <section className="padding bg-pale-blue">
+        <CustomerReviews />
+      </section>
+      <section className="padding-x sm:py-32 py-16 w-full">
+        <Subscribe />
+      </section>
+      <section className="bg-black padding-x padding-t pb-8">
+        <Footer />
+      </section>
     </main>
   );
 }
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Footer from "./Footer";
+import CustomerReviews from "./CustomerReviews";
+import PopularProducts from "./PopularProducts";
+import Services from "./Services";
+import SpecialOffers from "./SpecialOffers";
+import Subscribe from "./Subscribe";
+import SuperQuality from "./SuperQuality";
+
+export {
+  Hero,
+  Footer,
+  CustomerReviews,
+  PopularProducts,
+  Services,
+  SpecialOffers,
+  Subscribe,
+  SuperQuality,
+};
