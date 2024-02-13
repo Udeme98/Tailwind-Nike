35379dd7717c6e9b9908c9ae7feea0d1e4e0703d
Udeme98 <Darealudeme1@gmail.com>
2024-02-13 22:24:33 +0100

    working on the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,5 +1,10 @@
 const Hero = () => {
-  return <div>Hero</div>;
+  return (
+    <section
+      id="home"
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
+    ></section>
+  );
 };
 
 export default Hero;
