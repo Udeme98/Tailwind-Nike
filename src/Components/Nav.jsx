2e84ce45a7845134b6a4c2f786f875955f71254c
Udeme98 <Darealudeme1@gmail.com>
2024-02-13 11:39:04 +0100

    working on the navbar section

--- src/Components/Nav.jsx
+++ src/Components/Nav.jsx
@@ -1,5 +1,6 @@
 import { headerLogo } from "../assets/images";
 import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
 
 const Nav = () => {
   return (
@@ -8,7 +9,18 @@ const Nav = () => {
         <a href="/">
           <img src={headerLogo} alt="logo" width={130} height={29} />
         </a>
-        <ul className="flex-1 flex justify-center items-center gap-16 max-xl:hidden"></ul>
+        <ul className="flex-1 flex justify-center items-center gap-16 max-xl:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="font-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
       </nav>
     </header>
   );
