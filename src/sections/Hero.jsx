f4d8f34c499369784de20247aed0fbe94bc6a37b
Udeme98 <Darealudeme1@gmail.com>
2024-02-14 06:02:06 +0100

    working on the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -3,7 +3,9 @@ const Hero = () => {
     <section
       id="home"
       className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
-    ></section>
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28"></div>
+    </section>
   );
 };
 
