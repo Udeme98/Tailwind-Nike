16fca433058fce9f3de36839616480da3fc22792
Udeme98 <Darealudeme1@gmail.com>
2024-02-15 21:22:41 +0100

    styling the shoe card component

--- src/Components/ShoeCard.jsx
+++ src/Components/ShoeCard.jsx
@@ -1,5 +1,19 @@
-const ShoeCard = () => {
-  return <div>ShoeCard</div>;
+const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
+  const handleClick = () => {
+    if (bigShoeImg !== imgURL.bigShoe) {
+      changeBigShoeImage(imgURL.bigShoe);
+    }
+  };
+  return (
+    <div
+      className={`border-2 rounded-xl ${
+        bigShoeImg === imgURL ? "border-coral-red" : "border-transparent"
+      } cursor-pointer max-sm:flex-1`}
+      onClick={handleClick}
+    >
+      ShoeCard
+    </div>
+  );
 };
 
 export default ShoeCard;
