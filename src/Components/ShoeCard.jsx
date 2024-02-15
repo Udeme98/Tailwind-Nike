1802a3e92331be4fcc0255be43d3e714201d0cdd
Udeme98 <Darealudeme1@gmail.com>
2024-02-15 21:50:20 +0100

    styling the shoe card component

--- src/Components/ShoeCard.jsx
+++ src/Components/ShoeCard.jsx
@@ -4,6 +4,7 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
       changeBigShoeImage(imgURL.bigShoe);
     }
   };
+
   return (
     <div
       className={`border-2 rounded-xl ${
@@ -11,7 +12,15 @@ const ShoeCard = ({ imgURL, changeBigShoeImage, bigShoeImg }) => {
       } cursor-pointer max-sm:flex-1`}
       onClick={handleClick}
     >
-      ShoeCard
+      <div className="flex justify-center items-center bg-card bg-center bg-cover sm:w-40 sm:h-40 rounded-xl max-sm:p-4">
+        <img
+          src={imgURL.thumbnail}
+          alt="shoe collection"
+          width={127}
+          height={103}
+          className="object-contain"
+        />
+      </div>
     </div>
   );
 };
