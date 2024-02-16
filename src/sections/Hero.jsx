cbb295cb217b5f08ab7b2cf595191b5e63f2d562
Udeme98 <Darealudeme1@gmail.com>
2024-02-16 05:22:23 +0100

    styling the hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -45,7 +45,7 @@ const Hero = () => {
           height={500}
           className="object-contain relative z-10"
         />
-        <div>
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((shoe) => (
             <div key={shoe}>
               <ShoeCard
