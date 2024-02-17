07014a6db26008db1759d02d5400250103928cc3
Udeme98 <Darealudeme1@gmail.com>
2024-02-17 04:06:07 +0100

    created a popular product card component - added the card details

--- src/Components/PopularProductsCard.jsx
+++ src/Components/PopularProductsCard.jsx
@@ -1,9 +1,17 @@
-import React from "react";
+import { star } from "../assets/icons";
 
 export const PopularProductsCard = ({ imgURL, name, price }) => {
   return (
     <div className="flex flex-1 flex-col w-full max-sm:w-full">
       <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+      <div className="mt-8 flex justify-start gap-2.5">
+        <img src={star} alt="rating" width={24} height={24} />
+        <p className="text-xl font-montserrat leading-normal text-slate-gray">
+          (4.5)
+        </p>
+      </div>
+      <h3>{name}</h3>
+      <p>{price}</p>
     </div>
   );
 };
