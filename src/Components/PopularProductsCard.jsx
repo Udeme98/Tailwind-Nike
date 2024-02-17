d1ab01a10188898fd5b4af4be7fdfaf904380898
Udeme98 <Darealudeme1@gmail.com>
2024-02-17 03:45:28 +0100

    created a popular product card component

---
+++ src/Components/PopularProductsCard.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const PopularProductsCard = () => {
+  return <div>PopularProductsCard</div>;
+};
--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -12,6 +12,7 @@ const PopularProducts = () => {
           selections. Discover a world of comfort, design, and value
         </p>
       </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14"></div>
     </section>
   );
 };
