d3c3ccb53b2b12158c8e38bdc289d5a95e5c2b36
Udeme98 <Darealudeme1@gmail.com>
2024-02-17 03:39:12 +0100

    working on the popular products section - designing the text write up

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,7 +1,19 @@
 import { products } from "../constants";
 
 const PopularProducts = () => {
-  return <div>PopularProducts</div>;
+  return (
+    <section id="products" className="max-container max-sm:mt-12">
+      <div className="flex flex-col justify-start gap-5">
+        <h2 className="text-4xl font-palanquin font-bold">
+          Our <span className="text-coral-red">Popular</span> Products
+        </h2>
+        <p className="lg:max-w-lg mt-2 font-montserrat text-slate-gray">
+          Experience top-notch quality and style with our sought-after
+          selections. Discover a world of comfort, design, and value
+        </p>
+      </div>
+    </section>
+  );
 };
 
 export default PopularProducts;
