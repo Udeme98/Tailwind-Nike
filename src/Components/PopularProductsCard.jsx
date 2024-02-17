f6d876be09296f3a87779874eb57d72c83f34188
Udeme98 <Darealudeme1@gmail.com>
2024-02-17 03:56:53 +0100

    working on the popular products section - added images to the product section

--- src/Components/PopularProductsCard.jsx
+++ src/Components/PopularProductsCard.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export const PopularProductsCard = () => {
-  return <div>PopularProductsCard</div>;
+export const PopularProductsCard = ({ imgURL, name, price }) => {
+  return (
+    <div className="flex flex-1 flex-col w-full max-sm:w-full">
+      <img src={imgURL} alt={name} className="w-[280px] h-[280px]" />
+    </div>
+  );
 };
