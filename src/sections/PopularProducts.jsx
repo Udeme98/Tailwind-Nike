60565dd08abbe0c68176a5d282a44678d6705aa3
Udeme98 <Darealudeme1@gmail.com>
2024-02-17 03:50:14 +0100

    working on the popular products section - creating the product section

--- src/sections/PopularProducts.jsx
+++ src/sections/PopularProducts.jsx
@@ -1,3 +1,4 @@
+import { PopularProductsCard } from "../Components/PopularProductsCard";
 import { products } from "../constants";
 
 const PopularProducts = () => {
@@ -12,7 +13,11 @@ const PopularProducts = () => {
           selections. Discover a world of comfort, design, and value
         </p>
       </div>
-      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14"></div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-4 gap-14">
+        {products.map((product) => (
+          <PopularProductsCard key={product.name} {...product} />
+        ))}
+      </div>
     </section>
   );
 };
