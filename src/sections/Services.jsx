1ff57cd5351a01ee66d9f1223c1d4f2ed9438c91
Udeme98 <Darealudeme1@gmail.com>
2024-02-18 03:15:30 +0100

    building the Services section

--- src/sections/Services.jsx
+++ src/sections/Services.jsx
@@ -1,6 +1,13 @@
+import { services } from "../constants";
+import ServiceCard from "../Components/ServiceCard";
+
 const Services = () => {
   return (
-    <section className="flex max-container justify-center flex-wrap gap-9"></section>
+    <section className="flex max-container justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
   );
 };
 
