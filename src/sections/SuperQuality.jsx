bc57a1c242581b3c51fc70ee5f9babbeb63c7b33
Udeme98 <Darealudeme1@gmail.com>
2024-02-18 02:42:13 +0100

    building the Super Quality section

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -1,5 +1,24 @@
+import Button from "../Components/Button";
+
 const SuperQuality = () => {
-  return <section id="about-us">SuperQuality</section>;
+  return (
+    <section
+      id="about-us"
+      className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
+    >
+      <div className="flex flex-1 flex-col">
+        <h2 className="font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+          We provide you <span className="text-coral-red">Super</span>
+          <span className="text-coral-red"> Quality</span> Shoes
+        </h2>
+        <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14  sm:max-w-sm">
+          Discover Stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
+        </p>
+        <Button label="View Details" />
+      </div>
+    </section>
+  );
 };
 
 export default SuperQuality;
