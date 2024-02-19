cbb39e93e664ceb8c5a3578492f6403152cc036a
Udeme98 <Darealudeme1@gmail.com>
2024-02-19 12:24:37 +0100

    building and styling the subscribe section

--- src/sections/Subscribe.jsx
+++ src/sections/Subscribe.jsx
@@ -1,5 +1,12 @@
 const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section
+      id="contact-us"
+      className="max-container flex justify-between items-center max-lg:flex-col gap-10"
+    >
+      Subscribe
+    </section>
+  );
 };
 
 export default Subscribe;
