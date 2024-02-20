97aac5021af9bd53300535174fa411b58124bf99
Udeme98 <Darealudeme1@gmail.com>
2024-02-20 08:04:09 +0100

    building and styling the footer section

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -1,5 +1,21 @@
+import { footerLogo } from "../assets/images";
+
 const Footer = () => {
-  return <section>Footer</section>;
+  return (
+    <section className="max-container">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="flex flex-col items-start">
+          <a href="/">
+            <img src={footerLogo} width={150} height={46} />
+          </a>
+          <p className="mt-6 text-base text-white-400 leading-7 font-montserrat sm:max-w-sm">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            your perfect Size In Store. Get Rewards
+          </p>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Footer;
